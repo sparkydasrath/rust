b197d574f1a293021f3047911608b796c29b8a3e
sparky dasrath <sdasrath@gmail.com>
2021-06-26 17:11:36 -0400

    experimenting

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+E:\repos\rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+E:\repos\rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+E:\repos\rust\hello_cargo\target\debug\hello_cargo.exe: E:\repos\rust\hello_cargo\src\main.rs
