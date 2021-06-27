47b2a10568a96ed473114cf5a263afa135cc3541
sparky dasrath <sdasrath@gmail.com>
2021-06-27 00:09:51 -0400

    3.1 variables, let, mut, const, shadowing

--- rust_language/3_programming_concepts/variables/src/main.rs
+++ rust_language/3_programming_concepts/variables/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // variables
+    let mut x = 5;
+    println!("value of x = {}", x);
+    x = 6;
+    println!("value of x = {}", x);
+
+    // shadowing
+    let a = 5;
+    let a = a + 1;
+    let a = a * 2;
+    println!("value of a = {}", a);
+
+    
 }
