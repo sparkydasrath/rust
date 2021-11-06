021778052377765f5ccc2105da2fb45660e8c102
sparky dasrath <sdasrath@gmail.com>
2021-11-06 15:19:41 -0400

    Added test and implementation of search

--- rust_language/12_io_project/minigrep/src/lib.rs
+++ rust_language/12_io_project/minigrep/src/lib.rs
@@ -3,6 +3,9 @@ use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
 }
 
@@ -20,6 +23,16 @@ impl Config {
         let filename = args[2].clone();
         Ok(Config { query, filename })
     }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
 }
 
 #[cfg(test)]
