50e80accf958226d43a38fe2e38d079a4df5c3d1
sparky dasrath <sdasrath@gmail.com>
2021-11-06 14:50:39 -0400

    adding test module

--- rust_language/12_io_project/minigrep/src/lib.rs
+++ rust_language/12_io_project/minigrep/src/lib.rs
@@ -3,8 +3,6 @@ use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
     Ok(())
 }
 
@@ -21,5 +19,20 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
         Ok(Config { query, filename })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
