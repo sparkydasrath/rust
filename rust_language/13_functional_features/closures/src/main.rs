7129e4d170013050ee337c20671e668a5a1bedb7
sparky dasrath <sdasrath@gmail.com>
2021-11-07 09:56:36 -0500

    assign long running method to variable and call only once

--- rust_language/13_functional_features/closures/src/main.rs
+++ rust_language/13_functional_features/closures/src/main.rs
@@ -4,10 +4,11 @@ fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
 
-    generate_workout(simulated_user_specified_value, simulated_random_number);
+    generate_workout_without_closure(simulated_user_specified_value, simulated_random_number);
+    generate_workout_with_closure(simulated_user_specified_value, simulated_random_number);
 }
 
-fn generate_workout(intensity: u32, random_number: u32) {
+fn generate_workout_without_closure(intensity: u32, random_number: u32) {
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
@@ -25,6 +26,21 @@ fn generate_workout(intensity: u32, random_number: u32) {
                 "Today, run for {} minutes!",
                 simulated_expensive_calculation(intensity)
             );
+        }
+    }
+}
+
+fn generate_workout_with_closure(intensity: u32, random_number: u32) {
+    let expensive_result = simulated_expensive_calculation(intensity);
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_result);
+        println!("Next, do {} situps!", expensive_result);
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!("Today, run for {} minutes!", expensive_result);
         }
     }
 }
