a95d1d05152900cdeb330edc47b2b807cb918a75
sparky dasrath <sdasrath@gmail.com>
2021-11-07 10:16:45 -0500

    Added closure to generate workout

--- rust_language/13_functional_features/closures/src/main.rs
+++ rust_language/13_functional_features/closures/src/main.rs
@@ -9,9 +9,12 @@ fn main() {
         simulated_user_specified_value,
         simulated_random_number,
     );
+    generate_workout_with_closure(simulated_user_specified_value, simulated_random_number);
 }
 
 fn generate_workout_without_closure(intensity: u32, random_number: u32) {
+    println!("\nWithout closure");
+
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
@@ -33,7 +36,30 @@ fn generate_workout_without_closure(intensity: u32, random_number: u32) {
     }
 }
 
+fn generate_workout_with_closure(intensity: u32, random_number: u32) {
+    println!("\nWith closure");
+
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!("Today, run for {} minutes!", expensive_closure(intensity));
+        }
+    }
+}
+
 fn generate_workout_without_closure_but_better(intensity: u32, random_number: u32) {
+    println!("\nWithout closure better");
+
     let expensive_result = simulated_expensive_calculation(intensity);
 
     if intensity < 25 {
