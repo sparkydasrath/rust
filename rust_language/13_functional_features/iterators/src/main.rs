559822f11e9245445a3a2a7076dfb8f3fe315096
sparky dasrath <sdasrath@gmail.com>
2021-11-07 20:51:19 -0500

    itertaor consumer

--- rust_language/13_functional_features/iterators/src/main.rs
+++ rust_language/13_functional_features/iterators/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Iterators");
+
+    let v1 = vec![1, 2, 3];
+
+    // create iterator
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        // use iterator
+        /*
+        when we used a for loop because the loop took
+        ownership of v1_iter and made it mutable behind the scenes
+        */
+        println!("Got: {}", val);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn itertator_sum() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+        let total: i32 = v1_iter.sum();
+        assert_eq!(total, 6);
+    }
 }
