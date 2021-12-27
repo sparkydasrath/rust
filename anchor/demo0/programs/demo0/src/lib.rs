d1bad262cb4bb6dd6d57c76fceee1e7e8574c390
sparky dasrath <sdasrath@gmail.com>
2021-12-27 01:50:38 -0500

    WIll drop this and restart

--- anchor/demo0/programs/demo0/src/lib.rs
+++ anchor/demo0/programs/demo0/src/lib.rs
@@ -6,10 +6,23 @@ declare_id!("8JakaAVcvKE5Ajo79Taa67wyYvWB7kaX6GjwWxp6Cud7");
 #[program]
 pub mod demo0 {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn deposit(ctx: Context<Deposit>) -> ProgramResult {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Deposit<'info> {
+    #[account(init, payer = user, space = 32 + 32 + 1)]
+    pub demo_account: Account<'info, DemoAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct DemoAccount {
+    pub from: Pubkey,
+    pub to: Pubkey,
+    amount: u8,
+}
