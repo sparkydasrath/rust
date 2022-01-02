e6797474589b51662a10d9a061544b17532768eb
sparky dasrath <sdasrath@gmail.com>
2022-01-02 16:55:33 -0500

    Added test cases

--- anchor/demo01/programs/demo01/src/lib.rs
+++ anchor/demo01/programs/demo01/src/lib.rs
@@ -1,14 +1,43 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("BWTsZXDnSusYAdeyqVg2Rgt593HxQw14E8ox3U5mf7So");
 
 #[program]
 pub mod demo01 {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn initialize_deposit_account(ctx: Context<InitializeDepositAccount>) -> ProgramResult {
+        let deposit_account = &mut ctx.accounts.deposit_account;
+        deposit_account.authority = *ctx.accounts.authority.key;
+        deposit_account.amount = 0;
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u8) -> ProgramResult {
+        let deposit_account = &mut ctx.accounts.deposit_account;
+        deposit_account.amount += amount;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeDepositAccount<'info> {
+    #[account(init, payer = authority, space = 8 + 32 + 64)]
+    pub deposit_account: Account<'info, DepositAccount>,
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut, has_one = authority)]
+    pub deposit_account: Account<'info, DepositAccount>,
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct DepositAccount {
+    // this will be the payer for the DepositAccount
+    pub authority: Pubkey,
+    pub amount: u8,
+}
--- anchor/demo01/tests/demo01.ts
+++ anchor/demo01/tests/demo01.ts
@@ -1,17 +1,49 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { Demo01 } from '../target/types/demo01';
+import { Keypair, SystemProgram } from "@solana/web3.js";
+import assert = require("assert");
 
 describe('demo01', () => {
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
-
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  let _depositAccount;
   const program = anchor.workspace.Demo01 as Program<Demo01>;
 
   it('Is initialized!', async () => {
     // Add your test here.
-    const tx = await program.rpc.initialize({});
+    let depositAccount = anchor.web3.Keypair.generate();
+    const tx = await program.rpc.initializeDepositAccount({
+      accounts:{
+        depositAccount: depositAccount.publicKey,
+        authority: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      },
+      signers: [depositAccount]
+    });
+    console.log("ProgramId = ", SystemProgram.programId);
+    console.log("depositAccount = ", depositAccount);
     console.log("Your transaction signature", tx);
+
+    // get account from cluster
+    const account = await program.account.depositAccount.fetch(depositAccount.publicKey);
+
+    console.log("Account from cluster", account);
+
+    _depositAccount = depositAccount;
+  });
+
+  it('Deposit 1 unit', async () => {
+    const tx = await program.rpc.deposit(1, {
+      accounts:{
+        depositAccount: _depositAccount.publicKey,
+        authority: provider.wallet.publicKey,
+      },
+    });
+
+    const testAccount = await program.account.depositAccount.fetch(_depositAccount.publicKey);
+    console.log("Test account = ", testAccount.amount);
+    assert.ok(testAccount.amount.toString() == 1) ;
   });
 });
