8193ac7a8e1a94d62cb881f21b3165a71c0a110c
sparky dasrath <sdasrath@gmail.com>
2022-01-09 21:14:40 -0500

    Doing more with the demo

---
+++ anchor/demo03/tests/demo03.ts
@@ -0,0 +1,48 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { Demo03 } from '../target/types/demo03';
+import { SystemProgram } from "@solana/web3.js";
+
+let _baseAccount = null;
+
+describe('demo03', () => {
+
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  it("It initializes the account", async () => {
+    const program = anchor.workspace.Example2;
+    const baseAccount = anchor.web3.Keypair.generate();
+    await program.rpc.initialize("Hello World", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Data: ', account.data);
+    assert.ok(account.data === "Hello World");
+    _baseAccount = baseAccount;
+
+  });
+
+  it("Updates a previously created account", async () => {
+    const baseAccount = _baseAccount;
+    const program = anchor.workspace.Example2;
+
+    await program.rpc.update("Some new data", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Updated data: ', account.data)
+    assert.ok(account.data === "Some new data");
+    console.log('all account data:', account)
+    console.log('All data: ', account.dataList);
+    assert.ok(account.dataList.length === 2);
+  });
+});
