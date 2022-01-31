3a859f4e5315787cd55a96a6be86c15cfc06df94
sparky dasrath <sdasrath@gmail.com>
2022-01-31 10:56:58 -0500

    Set up transfer again

--- anchor/demo07/programs/demo07/src/lib.rs
+++ anchor/demo07/programs/demo07/src/lib.rs
@@ -4,11 +4,39 @@ declare_id!("B3hTk9gzsg5UQnHvzDfEmVwzMoQeB8Hoyao3YzmxRAPE");
 
 #[program]
 pub mod demo07 {
+    use anchor_lang::solana_program::native_token::LAMPORTS_PER_SOL;
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn do_transfer(ctx: Context<SimpleTransfer>, amount:u64) -> ProgramResult {
+        let from_account = &mut ctx.accounts.from_account;
+        let to_account = &mut ctx.accounts.payer;
+        from_account.amount = amount;
+
+
+        let fa_ai = from_account.to_account_info();
+        let to_ai = to_account.to_account_info();
+
+        let tx = anchor_lang::solana_program::system_instruction::transfer(
+            fa_ai.key,
+            to_ai.key,
+            amount * LAMPORTS_PER_SOL
+        );
+
+        anchor_lang::solana_program::program::invoke(&tx,&[fa_ai, to_ai]);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SimpleTransfer<'info> {
+    #[account(init, payer = payer, space = 8 + 8)]
+    pub from_account: Account<'info,Payload>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>
+
+}
+
+#[account]
+pub struct Payload{
+    pub amount: u64
+}
--- anchor/demo07/tests/demo07.ts
+++ anchor/demo07/tests/demo07.ts
@@ -1,17 +1,49 @@
 import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
+import {BN, Program, web3} from '@project-serum/anchor';
 import { Demo07 } from '../target/types/demo07';
+
+
+async function airDrop(provider, address, sol_amount){
+  console.log(`Airdropping ${sol_amount} lamports to UseAccount/Wallet ${address}`);
+  let airDropSig = await provider.connection.requestAirdrop(address, sol_amount * web3.LAMPORTS_PER_SOL)
+  // Confirming that the airdrop went through
+  let airDropTx = await provider.connection.confirmTransaction(airDropSig);
+  console.log(`Airdropped signature ${airDropTx.value}`);
+}
 
 describe('demo07', () => {
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  let provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Demo07 as Program<Demo07>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
+  it('Do transfer', async () => {
+
+    const from_account = web3.Keypair.generate();
+    await airDrop(provider, from_account.publicKey, 2*web3.LAMPORTS_PER_SOL);
+
+    const to_account = provider.wallet;
+
+    console.log("Balance before tx");
+    const from_bal_a = provider.connection.getBalance(from_account.publicKey);
+    const to_bal_a = provider.connection.getBalance(to_account.publicKey);
+    console.log(from_bal_a + "\n" + to_bal_a);
+    const tx = await program.rpc.doTransfer(new BN(1.5),
+        {
+          accounts:{
+            fromAccount: from_account.publicKey,
+            payer: to_account.publicKey,
+            systemProgram: program.programId
+          },
+          signers: [from_account]
+        });
     console.log("Your transaction signature", tx);
+
+    console.log("Balance before tx");
+    const from_bal_b = provider.connection.getBalance(from_account.publicKey);
+    const to_bal_b = provider.connection.getBalance(to_account.publicKey);
+    console.log(from_bal_b + "\n" + to_bal_b);
   });
 });
