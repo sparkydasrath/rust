7d5fefbbf983b1af24193ab74858da6d1489cc32
sparky dasrath <sdasrath@gmail.com>
2022-02-13 19:17:55 -0500

    Who fucking knows anymore

--- anchor/demo08/programs/demo08/src/lib.rs
+++ anchor/demo08/programs/demo08/src/lib.rs
@@ -1,14 +1,111 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::program::invoke_signed;
+use anchor_lang::solana_program::system_instruction;
 
 declare_id!("H9KVjYvKVJar2daLSGE4o4nQYN5R3YqGAXJLrVD7KLCP");
 
 #[program]
 pub mod demo08 {
+    use anchor_lang::solana_program::system_program;
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn is_hash_valid(hash: u8) -> bool {
+        // want to enforce that only even number hash can deposit or withdraw
+        if hash % 2 == 0 {
+            return true;
+        }
+        return false;
+    }
+
+    // deposit into the pda account
+    pub fn deposit(ctx: Context<Transfer>, amount: u64, bump: u8, hash: u8) -> ProgramResult {
+        let from = *ctx.accounts.client.key;
+        let to = *ctx.accounts.pda.key;
+        let program_name_seed = "demo08";
+        let ix = system_instruction::transfer(
+            &from, &to, amount);
+
+        let tx: ProgramResult = invoke_signed(
+            &ix,
+            &[
+                ctx.accounts.client.to_account_info(),  //from
+                ctx.accounts.pda.to_account_info(),     // to
+                ctx.accounts.system_program.to_account_info()
+            ],
+            &[
+                &[
+                    program_name_seed.as_bytes(),
+                    ctx.accounts.client.key.as_ref(),
+                    &[bump]
+                ]],
+        );
+        msg!(tx);
+        Ok(())
+    }
+
+    // deposit into the pda account
+    pub fn deposit_no_bump(ctx: Context<Transfer>, amount: u64, hash: u8) -> ProgramResult {
+
+        let is_hash_valid = is_hash_valid(hash);
+
+                
+        let from = *ctx.accounts.client.key;
+        let to = *ctx.accounts.pda.key;
+        let program_name_seed = "demo08";
+        let ix = system_instruction::transfer(
+            &from, &to, amount);
+
+        let tx: ProgramResult = system_program::invoke(
+            &ix,
+            &[
+                ctx.accounts.client.to_account_info(),  //from
+                ctx.accounts.pda.to_account_info(),     // to
+                ctx.accounts.system_program.to_account_info()
+            ]
+        );
+        msg!(tx);
+        Ok(())
+    }
+
+    // withdraw from pda and deposit into client account
+    pub fn withdraw(ctx: Context<Transfer>, amount: u64, bump: u8, hash: u8) -> ProgramResult {
+        let from = *ctx.accounts.pda.key;
+        let to = *ctx.accounts.client.key;
+        let program_name_seed = "demo08";
+        let ix = system_instruction::transfer(
+            &from, &to, amount);
+
+        let result = invoke_signed(
+            &ix,
+            &[
+                ctx.accounts.pda.to_account_info(),         //from
+                ctx.accounts.client.to_account_info(),      // to
+                ctx.accounts.system_program.to_account_info()
+            ],
+            &[
+                &[
+                    program_name_seed.as_bytes(),
+                    ctx.accounts.client.key.as_ref(),
+                    &[bump]
+                ]],
+        );
+        msg!(result);
         Ok(())
     }
 }
 
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Transfer<'info> {
+    #[account(mut)]
+    pub client: AccountInfo<'info>,
+    #[account(mut)]
+    pub pda: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error]
+pub enum Demo08Errors {
+    #[msg("Hash not valid")]
+    HashError
+}
--- anchor/demo08/tests/demo08.ts
+++ anchor/demo08/tests/demo08.ts
@@ -1,17 +1,65 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { Demo08 } from '../target/types/demo08';
+import * as solana from '@solana/web3.js'
+import {expect} from 'chai';
+
 
 describe('demo08', () => {
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Demo08 as Program<Demo08>;
 
   it('Is initialized!', async () => {
+    // helper functions
+    async function getAccountBalance(pubkey){
+      let account = await provider.connection.getAccountInfo(pubkey);
+      console.log(`Account info: ${account}`);
+      return account?.lamports ?? 0;
+    }
+
+    function expectBalance(actual, expected, message, slack=20000){
+      expect(actual, message).within(expected - slack, expected + slack)
+    }
+
+    async function createUser(airdropAmount) : Promise<anchor.web3.Keypair> {
+      airdropAmount = airdropAmount ?? 10 * anchor.web3.LAMPORTS_PER_SOL;
+      let user = anchor.web3.Keypair.generate();
+      // airdrop some sols to the user we just created
+      let sig = await provider.connection.requestAirdrop(user.publicKey, airdropAmount);
+      // verify that the sols are in the user account
+      await provider.connection.confirmTransaction(sig);
+
+      // print out the user's balance for good measure
+      let balance = await getAccountBalance(user.publicKey);
+      console.log(`Created user (${user.publicKey}) with ${balance} balance`);
+      return user;
+    }
+
+    async function getPdaAndBump(userAccount: anchor.web3.PublicKey){
+      const programNameSeed = "demo08";
+      let [pda, bump] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(programNameSeed), userAccount.toBytes()],program.programId);
+      return {pda: pda, bump: bump};
+    }
+
+
+    // end helper
+
+
     // Add your test here.
     const tx = await program.rpc.initialize({});
     console.log("Your transaction signature", tx);
+
+    it("Transfer from client to pda", async () => {
+      const clientAccount = await createUser(5)// create client and airdrop 5 sols
+      const pdaAccountAndBump = await getPdaAndBump( clientAccount.publicKey);
+
+
+
+    });
+
   });
 });
