b23add9c89018175c19f3f6f9d15f3dc9f2d2f76
sparky dasrath <sdasrath@gmail.com>
2022-02-20 23:50:21 -0500

    Tried following the example to use the MintTo approach but still getting type mismatch error

--- anchor/token01/programs/token01/src/lib.rs
+++ anchor/token01/programs/token01/src/lib.rs
@@ -7,7 +7,7 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod token01 {
     use super::*;
 
-    pub fn initialize(ctx: Context<InitializeMyMint>) -> ProgramResult {
+    pub fn initialize_my_mint(ctx: Context<InitializeMyMint>) -> ProgramResult {
         let mint_authority = *ctx.accounts.payer;
         let decimals = *ctx.accounts.mint.decimals;
         let token_program = *ctx.accounts.token_program;
@@ -32,7 +32,31 @@ pub mod token01 {
             decimals,
             mint_authority.key,
             Option::from(mint_authority.key),
+        )?;
+        Ok(())
+    }
+
+    pub fn my_mint_to(ctx: Context<MyMintTo>, mint_bump: u8) -> ProgramResult {
+        let mint = *ctx.accounts.mint;
+        let token_account = *ctx.accounts.token_account;
+        let mint_authority = *ctx.accounts.payer;
+        let token_program = *ctx.accounts.token_program;
+
+        let mint_accounts = anchor_spl::token::MintTo {
+            mint: mint.to_account_info(),
+            to: token_account.to_account_info(),
+            authority: mint_authority.to_account_info(),
+        };
+
+        let cpi_context = CpiContext::new_with_signer(
+            token_program.to_account_info(),
+            mint_accounts,
+            &[&[&"token01-my-mint-to".as_bytes(), &[mint_bump]]],
         );
+
+        // want to just create the mint and token account but don't do anything hence the 0 amount
+        let init_mint_with_token_account = anchor_spl::token::mint_to(cpi_context, 0)?;
+        Ok(())
     }
 }
 
@@ -47,6 +71,24 @@ pub struct InitializeMyMint<'info>{
     pub mint : Account<'info, Mint>,
     #[account(mut)]
     pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+#[instruction(mint_bump: u8)]
+pub struct MyMintTo<'info>{
+    /* From the doc https://github.com/project-serum/anchor/blob/a83064420363940a8a0824229634018b4bca253e/spl/src/token.rs#L35 
+    We need to call CpiContext with the MintTo struct 
+    */
+    #[derive(init, payer = payer, seeds = [b"token01-my-mint-to".as_ref()], bump = mint_bump,
+        mint::decimals = 9,
+        mint::authority = payer)]
+    pub mint : Account<'info, Mint>,
+    #[account(mut)]
+    pub payer: Signer<'info>, // account that owns the mint (not the Token Program)
+    #[derive(init_if_needed, payer = payer, mint::decimals = 9, mint::authority = payer)]
+    pub token_account: Account<'info, TokenAccount>, // account to tx tokens to; create if needed
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
 }
